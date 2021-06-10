ed0204e23a4d26d200a6fc4118397530fc03c85c
Reshma1007 <85442105+Reshma1007@users.noreply.github.com>
2021-06-10 09:47:38 +0530

    Update ser.py

--- ser.py
+++ ser.py
@@ -2,8 +2,8 @@ import socket
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((socket.gethostname(),1234))
 s.listen(5)
-while True;
-clientsocket, address=s.accept()
-print(f"connection from {address}has beeb established!")
-clientsocket.send(bytes("welcome to the server!","ütf-8"))
+while True:
+    clientsocket, address=s.accept()
+    print(f"connection from {address} has been established!")
+    clientsocket.send(bytes("welcome to the server!","ütf-8"))
 
